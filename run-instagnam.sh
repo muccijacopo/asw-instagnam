b4edd1b1e7efb4d1b8691d2ab8e6a7c80a484d04
jacopomucci <mucci.jacopo@gmail.com>
2020-05-24 21:35:22 +0200

    dockerfile e docker-compose per ricette, ricette-db e consul

--- run-instagnam.sh
+++ run-instagnam.sh
@@ -2,12 +2,12 @@
 
 # Script per avviare l'applicazione Instagnam 
 
-echo Running INSTAGNAM 
+echo Running INSTAGNAM
 
-# Consul deve essere avviato separatamente 
+gradle build && docker-compose up
 
-java -Xms64m -Xmx128m -jar ricette/build/libs/ricette.jar &
-java -Xms64m -Xmx128m -jar connessioni/build/libs/connessioni.jar &
-java -Xms64m -Xmx128m -jar ricette-seguite/build/libs/ricette-seguite.jar &
-
-java -Xms64m -Xmx128m -jar api-gateway/build/libs/api-gateway.jar &
+#java -Xms64m -Xmx128m -jar ricette/build/libs/ricette.jar &
+#java -Xms64m -Xmx128m -jar connessioni/build/libs/connessioni.jar &
+#java -Xms64m -Xmx128m -jar ricette-seguite/build/libs/ricette-seguite.jar &
+#
+#java -Xms64m -Xmx128m -jar api-gateway/build/libs/api-gateway.jar &
