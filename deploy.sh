a21898b2c42a3edb0d7348e3b094f0f85a0709d5
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2020-05-26 08:39:48 +0200

    Add gradle build Add Dockerfile in ricette-seguite Add zookeeper and kafka

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@ CURPATH=$(pwd)
 echo "removing old containers"
 docker-compose -f docker-compose.yml down --remove-orphans
 
+echo "cleaning springboot apps"
+gradle clean
+if [ $? != 0 ] ; then
+	exit
+fi
+
 echo "compiling springboot apps"
 gradle build
 if [ $? != 0 ] ; then
@@ -12,7 +18,7 @@ if [ $? != 0 ] ; then
 fi
 
 echo "running new containers"
-docker-compose -f docker-compose.yml up
+docker-compose -f docker-compose.yml up --build
 if [ $? != 0 ] ; then
 	exit
 fi
