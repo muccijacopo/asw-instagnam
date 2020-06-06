113d0298d315105e6ab60369eca12269c2c5ffe0
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2020-06-06 19:07:43 +0200

    add docker-compose build and push add script build and push images

---
+++ docker-build-and-push-images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CURPATH=$(pwd)
+
+echo "building springboot apps"
+gradle clean build
+if [ $? != 0 ] ; then
+	exit
+fi
+
+echo "building containers"
+docker-compose -f docker-compose-images.yml build
+
+echo "pushing containers"
+docker-compose -f docker-compose-images.yml push
