1ddfe570d84ea5ae33a268b37f7c4a30527697cf
jacopomucci <mucci.jacopo@gmail.com>
2020-06-09 20:12:58 +0200

    init ricette db

---
+++ ricette/init/init-ricette-db.sql
@@ -0,0 +1,9 @@
+-- CREATE DATABASE IF NOT EXISTS instagnam;
+-- USE instagnam;
+-- CREATE TABLE IF NOT EXISTS ricetta_completa;
+-- INSERT INTO ricetta_completa VALUES("Cristiano", "Panino al prosciutto", "Metti il prosciutto nel panino");
+-- INSERT INTO ricetta_completa VALUES("Cristiano", "Pizza e mortazza", "Metti la mortadella nella pizza");
+-- INSERT INTO ricetta_completa VALUES("Gennaro", "Tonno e fagioli", "Unisci i fagioli con il tonno");
+-- INSERT INTO ricetta_completa VALUES("Antonino", "Pizza margherita", "... e alla fine inforna" );
+-- INSERT INTO ricetta_completa VALUES("Benedetta", "Tonno e fagioli", "Unisci il tonno con i fagioli");
+CREATE DATABASE prova;
